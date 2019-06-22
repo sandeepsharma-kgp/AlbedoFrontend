df3c178792d08d29ef49944d79f6b5321ac9a9c3
gauravtirodkar <gauravtirodkar42@gmail.com>
2019-06-22 11:53:01 +0530

    First Update

--- albedo/src/App.vue
+++ albedo/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <LandingPage />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LandingPage from './components/LandingPage'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    LandingPage
   }
 }
 </script>
@@ -23,6 +22,7 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 0px;
+  padding : 0px;
 }
 </style>
---
+++ albedo/src/components/LandingPage.vue
@@ -0,0 +1,31 @@
+<template>
+<div>
+    <b-navbar toggleable="lg" type="dark" variant="info">
+    <b-navbar-brand href="#">Albedo Academy</b-navbar-brand>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+          <b-img :src="require('../static/icons/map-pin-marked.svg')" style="padding : 14px;"></b-img>
+          <b-img :src="require('../static/icons/man-user.svg')"></b-img>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+.navbar {
+    background-color : rgb(240, 109, 61) !important;
+}
+
+</style>
