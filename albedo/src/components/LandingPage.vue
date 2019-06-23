c837ed146dd5173f609fd020a9cddcddb1fb7ae9
pd <gayatripatil5519@gmail.com>
2019-06-23 18:27:21 +0530

    navbar created

---
+++ albedo/src/components/LandingPage.vue
@@ -0,0 +1,54 @@
+<template>
+<div>
+    <b-navbar toggleable="lg" type="dark" variant="info">
+    <b-navbar-brand href="#">Albedo Academy</b-navbar-brand>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">  
+          <a href="#" class = "call"><b-img :src="require('../static/icons/call.svg')" style="padding : 14px; height: 50px; width: 50px"></b-img><h8 style="padding : 14px; padding-left:0px;">+91-xxxxxxxxxx</h8></a>
+          <b-button variant="outline-dark" class="scholar">Scholarship</b-button>
+          <b-nav-item href="#" style="padding:5px;" active>About Us</b-nav-item>
+          <b-nav-item href="#" style="padding:5px;" active>Doubts & Solution</b-nav-item>
+          <b-nav-item-dropdown text="All Program" right type="dark" style="padding:5px;">
+            <b-dropdown-item href="#">EN</b-dropdown-item>
+            <b-dropdown-item href="#">ES</b-dropdown-item>
+            <b-dropdown-item href="#">RU</b-dropdown-item>
+            <b-dropdown-item href="#">FA</b-dropdown-item>
+          </b-nav-item-dropdown>
+          <b-img :src="require('../static/icons/man-user.svg')" style="padding : 14px; height: 50px; width: 50px"></b-img>
+          <b-img :src="require('../static/icons/map-pin-marked.svg')" style="padding : 14px; height: 50px; width: 50px"></b-img>
+          
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+.navbar {
+    background-color : rgb(240, 109, 61) !important;
+}
+.scholar{
+  background : none;
+  color :white;
+  border-radius : 50px;
+  border-color: white;
+}
+.call{
+  color : white;
+  text-decoration: none;
+}
+.call:hover{
+  color:white;
+}
+</style>
