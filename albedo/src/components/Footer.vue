47e520cccdbf59d82b371a165a75da35f1fb35dd
pd <gayatripatil5519@gmail.com>
2019-06-25 23:31:40 +0530

    contact Us button

--- albedo/src/components/Footer.vue
+++ albedo/src/components/Footer.vue
@@ -11,6 +11,7 @@
     <div class="copright">
         Copyright Notice: @ 2018 Greycells18 Media Limited and its Licensors. All rights reserved.
     </div>
+    <div class="fixed-bottom" style="text-align:right;padding:20px 40px;"><b-button style="background-color:#063b6c; border-color:#063b6c; border-radius:8px;"><b-img :src="require('../assets/icons/envelope.svg')" style="padding: 0px 5px 0px 0px;"></b-img>Contact Us</b-button></div>
 </div>
 </template>
 
